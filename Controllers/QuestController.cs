28488d97d10262f472bff9062f89d900647a47ce
stahsin00 <lamisa1998@hotmail.com>
2024-08-21 21:34:48 -0700

    complete quest

--- Controllers/QuestController.cs
+++ Controllers/QuestController.cs
@@ -5,8 +5,6 @@ using Microsoft.Extensions.Logging;
 using Solara.Data;
 using Solara.Models;
 using Solara.Dtos;
-using System.Text.Json;
-using System.IO;
 
 namespace Solara.Controllers
 {
@@ -34,7 +32,7 @@ namespace Solara.Controllers
 
                 if (string.IsNullOrEmpty(email))
                 {
-                    return BadRequest("Invalid email claim.");
+                    return BadRequest(new { message = "Invalid email claim." });
                 }
 
                 var user = await _context.Users.Include(u => u.Quests)  // TODO: consider pagination
@@ -44,7 +42,9 @@ namespace Solara.Controllers
                     return NotFound(new { message = "User not found." });
                 }
 
-                return Ok(user.Quests);
+                var quests = user.Quests.Where(q => !q.Complete).ToList();  // TODO: filter when retrieving from database
+
+                return Ok(quests);
             } catch (Exception e) {
                 _logger.LogError(e, "Error in QuestController - GetAllQuests:");
                 return StatusCode(500, new { message = "Unable to get quests." });
@@ -59,19 +59,19 @@ namespace Solara.Controllers
                 // TODO: validation
                 if (!Enum.TryParse<Difficulty>(dto.Difficulty, true, out var difficulty))
                 {
-                    return BadRequest("Invalid difficulty. Difficulty must be one of the following: Easy, Medium, Hard, Unspecified.");
+                    return BadRequest(new { message = "Invalid difficulty. Difficulty must be one of the following: Easy, Medium, Hard, Unspecified." });
                 }
 
                 if (!Enum.TryParse<Repetition>(dto.Repetition, true, out var repetition) && !string.IsNullOrEmpty(dto.Repetition))
                 {
-                    return BadRequest($"Invalid repetition type: {dto.Repetition}");
+                    return BadRequest(new { message = $"Invalid repetition type: {dto.Repetition}" });
                 }
                 
                 var email = User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Email)?.Value;
 
                 if (string.IsNullOrEmpty(email))
                 {
-                    return BadRequest("Invalid email claim.");
+                    return BadRequest(new { message = "Invalid email claim." });
                 }
 
                 var user = await _context.Users.Include(u => u.Quests)
@@ -117,6 +117,46 @@ namespace Solara.Controllers
             } catch (Exception e) {
                 _logger.LogError(e, "Error in QuestController - CreateQuest:");
                 return StatusCode(500, new { message = "Unable to create quest." });
+            }
+        }
+
+        // PATCH /api/quest/complete/{id}
+        [HttpPatch("complete/{id:int}")]
+        public async Task<IActionResult> CompleteQuest(int id)
+        {
+            try {
+                var email = User.Claims.FirstOrDefault(c => c.Type == System.Security.Claims.ClaimTypes.Email)?.Value;
+
+                if (string.IsNullOrEmpty(email))
+                {
+                    return BadRequest(new { message = "Invalid email claim." });
+                }
+
+                var user = await _context.Users.Include(u => u.Quests)
+                                            .FirstOrDefaultAsync(u => u.Email == email);
+                if (user == null)
+                {
+                    return NotFound(new { message = "User not found." });
+                }
+
+                var quest = user.Quests.FirstOrDefault(q => q.Id == id);
+                if (quest == null)
+                {
+                    return NotFound(new { message = "Quest not found." });
+                }
+
+                if (quest.Complete) {
+                    return BadRequest(new { message = "Quest is already completed." });
+                }
+
+                quest.Complete = true;
+                quest.UpdatedAt = DateTime.UtcNow;
+                await _context.SaveChangesAsync();
+
+                return Ok(new { message = "Quest completed." });
+            } catch (Exception e) {
+                _logger.LogError(e, "Error in QuestController - CompleteQuest:");
+                return StatusCode(500, new { message = "Unable to complete quest." });
             }
         }
     }
